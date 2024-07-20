a76d193f1115083cd73007c4ecc8e2cd4ffb57e4
Abolfazl Shahidi <shahidi2004shahidi@gmail.com>
2024-07-20 22:13:38 +0330

    implement services

---
+++ Cms/Data/Services/PageCommentService.cs
@@ -0,0 +1,68 @@
+﻿using Cms.Data.Context;
+using Cms.Data.Repositories;
+using Cms.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cms.Data.Services
+{
+    public class PageCommentService(CmsDbContext db) : IPageComment
+    {
+        private readonly CmsDbContext _context = db;
+
+        public List<PageComment> AllComments()
+        {
+            return [.. _context.PageComments];
+        }
+
+        public bool DeleteComment(int id)
+        {
+            try
+            {
+                var comment = GetPageCommentById(id);
+                _context.Entry(comment).State = EntityState.Deleted;
+                Save();
+                return true;
+            }
+            catch
+            {
+
+                return false;
+            }
+        }
+
+        public bool DeleteComment(PageComment comment)
+        {
+            throw new NotImplementedException();
+        }
+
+        public PageComment GetPageCommentById(int id)
+        {
+            var comment = _context.PageComments.Find(id);
+            return comment;
+        }
+
+        public bool InsertComment(PageComment comment)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<PageComment> PageComments(int id)
+        {
+            var comments = _context.PageComments.Where(p => p.PageId == id);
+            return [.. comments];
+        }
+
+        public bool Save()
+        {
+            try
+            {
+                _context.SaveChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Cms/Data/Services/PageGroupService.cs
@@ -0,0 +1,93 @@
+﻿using Cms.Data.Context;
+using Cms.Data.Repositories;
+using Cms.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cms.Data.Services
+{
+    public class PageGroupService(CmsDbContext db) : IPageGroup
+    {
+        private readonly CmsDbContext _context = db;
+        public List<PageGroup> AllGroups()
+        {
+            return [.. _context.PageGroups];
+        }
+
+        public bool DeleteGroup(int id)
+        {
+            var group = GetGroupById(id);
+            try
+            {
+                _context.Entry(group).State = EntityState.Deleted;
+                return true;
+            }
+            catch
+            {
+
+                return false;
+            }
+
+        }
+
+        public bool DeleteGroup(PageGroup group)
+        {
+            try
+            {
+                _context.Entry(group).State = EntityState.Deleted;
+                return true;
+            }
+            catch
+            {
+
+                return false;
+            }
+
+        }
+
+        public PageGroup GetGroupById(int id)
+        {
+            var group = _context.PageGroups.Find(id);
+            return group;
+        }
+
+        public bool InsertGroup(PageGroup group)
+        {
+            try
+            {
+                _context.Entry(group).State = EntityState.Added;
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool Save()
+        {
+            try
+            {
+                _context.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public bool UpdateGroup(int id)
+        {
+            var group = GetGroupById(id);
+            try
+            {
+                _context.Entry(group).State = EntityState.Modified;
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
